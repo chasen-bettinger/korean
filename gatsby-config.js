47aa237dce184cb53f02de94c49a19023efe894a
Chasen Bettinger <bettingerchasen@gmail.com>
2021-01-19 20:06:03 -0500

    first day done

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Korean Journal`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Chasen Bettinger`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
--- src/components/bio.js
+++ src/components/bio.js
@@ -7,7 +7,6 @@
 
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import Image from "gatsby-image"
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
@@ -41,16 +40,6 @@ const Bio = () => {
 
   return (
     <div className="bio">
-      {avatar && (
-        <Image
-          fixed={avatar}
-          alt={author?.name || ``}
-          className="bio-avatar"
-          imgStyle={{
-            borderRadius: `50%`,
-          }}
-        />
-      )}
       {author?.name && (
         <p>
           Written by <strong>{author.name}</strong> {author?.summary || null}
